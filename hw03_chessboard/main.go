1e1a93b23297579edc9504577b4bac690caaac48
EAD <e.a.dubina@gmail.com>
2023-09-04 23:39:08 +0300

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -6,12 +6,10 @@ func main() {
 	var size int
 	fmt.Println("ЗАДАЙТЕ РАЗМЕР ДОСКИ:")
 	fmt.Scanf("%d", &size)
-	fmt.Println()
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			fmt.Print("# ")
 		}
 		fmt.Println()
 	}
-
 }
