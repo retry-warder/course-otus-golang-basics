a70ee54a60d7f9f4fd74de7b0321a06114b38b47
EAD <e.a.dubina@gmail.com>
2023-09-10 20:12:21 +0300

    HW4 COMPLITE5

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	book1 := types.NewBook(1, "T1", "A1", 2022, 100, 10)
+	book1 := types.NewBook(1, "T3", "A1", 2022, 100, 10)
 	book2 := types.NewBook(2, "T2", "A2", 2023, 120, 15)
 	compare := types.NewCompareBook(types.Size)
 	compare.Compare(book1, book2)
