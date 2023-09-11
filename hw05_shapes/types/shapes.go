67bc224aa7d6cd7ecf4eda75dae9734e40bfa029
EAD <e.a.dubina@gmail.com>
2023-09-11 09:50:06 +0300

    HW05 FIXED_002

--- hw05_shapes/types/shapes.go
+++ hw05_shapes/types/shapes.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -11,62 +12,69 @@ type Shape interface {
 
 type Circle struct {
 	radius int
+	area   float64
 }
 
 func NewCircle(radius int) *Circle {
-	circle := Circle{radius}
+	circle := Circle{radius, float64(math.Pi) * math.Pow(float64(radius), 2)}
 	return &circle
 }
 
 func (c *Circle) Area() float64 {
-	area := float64(math.Pi) * math.Pow(float64(c.radius), 2)
-	fmt.Printf("Круг: радиус %d Площадь: %f", c.radius, area)
-	fmt.Println()
-	return area
+	return c.area
+}
+
+func (c *Circle) String() string {
+	tmplts := "Круг: радиус %d Площадь: %f"
+	return fmt.Sprintf(tmplts, c.radius, c.area)
 }
 
 type Triangle struct {
 	base   int
 	height int
+	area   float64
 }
 
 func NewTriangle(base int, height int) *Triangle {
-	triangle := Triangle{base, height}
+	triangle := Triangle{base, height, (float64(base) * float64(height)) / 2}
 	return &triangle
 }
 
 func (t *Triangle) Area() float64 {
-	area := (float64(t.base) * float64(t.height)) / 2
-	fmt.Printf("Треугольник: основание %d, высота %d Площадь: %f", t.base, t.height, area)
-	fmt.Println()
-	return area
+	return t.area
+}
+
+func (t *Triangle) String() string {
+	tmplts := "Треугольник: основание %d, высота %d Площадь: %f"
+	return fmt.Sprintf(tmplts, t.base, t.height, t.area)
 }
 
 type Rectangle struct {
 	length int
 	width  int
+	area   float64
 }
 
 func NewRectangle(length int, width int) *Rectangle {
-	rectangle := Rectangle{length, width}
+	rectangle := Rectangle{length, width, float64(length) * float64(width)}
 	return &rectangle
 }
 
 func (r Rectangle) Area() float64 {
-	area := float64(r.length) * float64(r.width)
-	fmt.Printf("Прямоугольник: ширина %d, высота %d Площадь: %f", r.length, r.width, area)
-	fmt.Println()
-	return area
+	return r.area
 }
 
-func CalculateArea(s any) float64 {
-	var area float64
+func (r Rectangle) String() string {
+	tmplts := "Прямоугольник: ширина %d, высота %d Площадь: %f"
+	return fmt.Sprintf(tmplts, r.length, r.width, r.area)
+}
+
+func CalculateArea(s any) (float64, error) {
 	shape, ok := s.(Shape)
 	if ok {
-		area = shape.Area()
-	} else {
-		fmt.Println("Ошибка: переданный объект не является фигурой.")
-		panic("Ошибка: переданный объект не является фигурой.")
+		fmt.Println(shape)
+		return shape.Area(), nil
 	}
-	return area
+	fmt.Println("Ошибка: переданный объект не является фигурой.")
+	return 0.0, errors.New("ошибка: переданный объект не является фигурой")
 }
