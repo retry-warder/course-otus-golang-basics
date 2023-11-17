7be3f9aa33d94de3437eea4fcba4b8d18100d527
EAD <e.a.dubina@gmail.com>
2023-11-18 02:49:57 +0300

    TEST_001

--- hw09_serialize/main_test.go
+++ hw09_serialize/main_test.go
@@ -55,56 +55,6 @@ func Test_MarshallJson(t *testing.T) {
 	}
 }
 
-func Test_UnmarshallJson(t *testing.T) {
-	b1 := types.NewBook(1, "Test 1", "Test.T.T", 2010, 1000, 10.10)
-	j1 := []byte(`{"id":1,"title":"Test 1","author":"Test.T.T","year":2010,"size":1000,"rate":10.1}`)
-
-	b2 := types.NewBook(1, "Test 2", "Test.X.Y", 2000, 1500, 10.90)
-	j2 := []byte(`{"id":1,"title":"Test 2","author":"Test.X.Y","year":2000,"size":1500,"rate":10.9}`)
-
-	var b3 types.Books
-	b3 = append(b3, b1, b2)
-	f, err := os.Open("test.json")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	j3, err := io.ReadAll(f)
-	if err != nil {
-		panic(err)
-	}
-
-	strtests := []struct {
-		name     string
-		input    []byte
-		expected string
-	}{
-		{"Ok Unmarshall json", j1, fmt.Sprintf("%v", b1)},
-		{"Ok Unmarshall json", j2, fmt.Sprintf("%v", b2)},
-		{"Ok Unmarshall slice json", j3, fmt.Sprintf("%v", b3)},
-	}
-
-	for _, tc := range strtests {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			if tc.name == "Ok Unmarshall slice json" {
-				var res types.Books
-				err = json.Unmarshal(tc.input, &res)
-				require.NoError(t, err)
-				require.Equal(t, tc.expected, fmt.Sprintf("%v", res),
-					fmt.Sprintf("input (%v) = %v; want %v", tc.input, res, tc.expected))
-			} else {
-				var res types.Book
-				err = json.Unmarshal(tc.input, &res)
-				require.NoError(t, err)
-				require.Equal(t, tc.expected, fmt.Sprintf("%v", res),
-					fmt.Sprintf("input (%v) = %v; want %v", tc.input, res, tc.expected))
-			}
-		})
-	}
-}
-
 func Test_MarshallPB(t *testing.T) {
 	strtests := []struct {
 		name     string
