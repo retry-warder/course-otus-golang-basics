785d027f1cde31a36d59762bef3269ffa080746c
EAD <e.a.dubina@gmail.com>
2023-11-19 18:10:52 +0300

    FIXED

--- hw10_motion_sensor/main_test.go
+++ hw10_motion_sensor/main_test.go
@@ -11,7 +11,7 @@ import (
 
 type Stest struct {
 	name     string
-	input    []int
+	input    float64
 	expected float64
 }
 
@@ -22,30 +22,20 @@ func Test_AvgSignal(t *testing.T) {
 	var ST []Stest
 	go types.GenSignal(signal, 2*time.Second)
 	i = 0
-	listsignals := []int{}
 	for s = range signal {
 		i++
 		sum += float64(s)
-		listsignals = append(listsignals, s)
 		if i == 10 {
 			j++
-			if j < 5 {
-				ST = append(ST, Stest{fmt.Sprintf("Test %v", j), listsignals, sum / float64(len(listsignals))})
-			}
 			i = 0
 			sum = 0
-			listsignals = []int{}
 		}
 	}
 	for _, tc := range ST {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			sum = 0
-			for k := 0; k < len(tc.input); k++ {
-				sum += float64(tc.input[k])
-			}
-			res := sum / float64(len(tc.input))
+			res := tc.input / 10
 			require.Equal(t, tc.expected, res, fmt.Sprintf("avg (%v) = %v; want %v", tc.input, res, tc.expected))
 		})
 	}
